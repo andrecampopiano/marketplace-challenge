f7604079f25dd221464cf749a44bb5475cee523b
Andre Campopiano <andre.campopiano@gmail.com>
2023-06-06 11:49:57 -0300

    [ColorExtension] Criar Extension UIColor

---
+++ MarketplaceChallenge/Sources/Infrastructure/DesignSystem/UIColors/Extension/UIColor.swift
@@ -0,0 +1,71 @@
+//
+//  UIColor.swift
+//  MarketplaceChallenge
+//
+//  Created by Andre Luis Campopiano on 06/06/23.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Primary Colors
+    
+    /// Primary Blue - Hex:  #0026D9
+    static var primaryBlue: UIColor { return Colors.primaryBlue.color }
+    
+    /// Primary Arctic Blue - Hex:  #99D6FF
+    static var primaryArcticBlue: UIColor { return Colors.primaryArcticBlue.color }
+    
+    /// Primary Light Blue - Hex:  #5270FE
+    static var primaryLightBlue: UIColor { return Colors.primaryLightBlue.color }
+    
+    /// Primary Sky Blue - Hex:  #3152EF
+    static var primarySkyBlue: UIColor { return Colors.primarySkyBlue.color }
+    
+    // MARK: - Secondary Colors
+    
+    /// Secondary Green - Hex: #6DD400
+    static var secondaryGreen: UIColor { return Colors.secondaryGreen.color }
+    
+    /// Secondary Red - Hex: #EB0028
+    static var secondaryRed: UIColor { return Colors.secondaryRed.color }
+    
+    /// Secondary Dark Blue - Hex: #1C0098
+    static var secondaryDarkBlue: UIColor { return Colors.secondaryDarkBlue.color }
+    
+    /// Secondary Dark Green - Hex: #00804D
+    static var secondaryDarkGreen: UIColor { return Colors.secondaryDarkGreen.color }
+    
+    /// Secondary Light Green - Hex: #E4F8CE
+    static var secondaryLightGreen: UIColor { return Colors.secondaryLightGreen.color }
+    
+    /// Secondary Light Yellow - Hex: #FEEAC2
+    static var secondaryLightYellow: UIColor { return Colors.secondaryLightYellow.color }
+    
+    /// Secondary Navy - Hex: #002E46
+    static var secondaryNavy: UIColor { return Colors.secondaryNavy.color }
+    
+    /// Secondary Yellow - Hex: #FAB932
+    static var secondaryYellow: UIColor { return Colors.secondaryYellow.color }
+    
+    // MARK: - Neutral Colors
+    
+    /// Neutral Dark Grey - Hex: #696969
+    static var neutralDarkGrey: UIColor { return Colors.neutralDarkGrey.color }
+    
+    /// Neutral Black - Hex: #000000
+    static var neutralBlack: UIColor { return Colors.neutralBlack.color }
+    
+    /// Neutral Grey - Hex: #BABABA
+    static var neutralGrey: UIColor { return Colors.neutralGrey.color }
+    
+    /// Neutral White - Hex: #FFFFFF
+    static var neutralWhite: UIColor { return Colors.neutralWhite.color }
+    
+    /// Neutral Light Grey - Hex: #F0F1F2
+    static var neutralLightGrey: UIColor { return Colors.neutralLightGrey.color }
+}
+
