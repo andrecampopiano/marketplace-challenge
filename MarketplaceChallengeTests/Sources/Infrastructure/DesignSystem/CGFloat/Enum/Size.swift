7da513ed988f205de46151cf49ee701ab85ada94
Andre Campopiano <andre.campopiano@gmail.com>
2023-06-06 11:31:54 -0300

    [CGFloat Extension] Criar Enum Size

---
+++ MarketplaceChallengeTests/Sources/Infrastructure/DesignSystem/CGFloat/Enum/Size.swift
@@ -0,0 +1,35 @@
+//
+//  Size.swift
+//  MarketplaceChallengeTests
+//
+//  Created by Andre Luis Campopiano on 06/06/23.
+//
+
+import Foundation
+
+public enum Size: CGFloat {
+    /// value: 4.0
+    case nano = 4.0
+    /// value: 8.0
+    case extraSmall = 8.0
+    /// value: 12.0
+    case smaller = 12.0
+    /// value: 16.0
+    case small = 16.0
+    /// value: 24.0
+    case medium = 24.0
+    /// value: 32.0
+    case big = 32.0
+    /// value: 40.0
+    case xBig = 40.0
+    /// value: 48.0
+    case large = 48.0
+    /// value: 56.0
+    case xLarge = 56.0
+    /// value: 64..0
+    case giga = 64.0
+    /// value:  72.0
+    case xGiga = 72.0
+    /// value: zero 0.0
+    case zero = 0.0
+}
