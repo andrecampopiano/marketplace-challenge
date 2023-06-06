4227493698c82d42071821b8bdaf6e80d936d4d1
Andre Campopiano <andre.campopiano@gmail.com>
2023-06-06 11:33:00 -0300

    [CGFloatExtension] Criar enum MarginSize

---
+++ MarketplaceChallengeTests/Sources/Infrastructure/DesignSystem/CGFloat/Enum/MarginSize.swift
@@ -0,0 +1,27 @@
+//
+//  MarginSize.swift
+//  MarketplaceChallengeTests
+//
+//  Created by Andre Luis Campopiano on 06/06/23.
+//
+
+import Foundation
+
+enum MarginSize: CGFloat {
+    /// value: 8.0
+    case nano = 8.0
+    /// value: 12.0
+    case verySmall = 12.0
+    /// value: 16.0
+    case small = 16.0
+    /// value: 24.0
+    case medium = 24.0
+    /// value: 28.0
+    case big = 28.0
+    /// value: 32.0
+    case veryBig = 32.0
+    /// value: 48.0
+    case macro = 48.0
+    /// value: zero 0.0
+    case zero = 0.0
+}
