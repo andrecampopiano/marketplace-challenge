e4b9b1049ac690e2ee2f5806c748e00c937d8348
Andre Campopiano <andre.campopiano@gmail.com>
2023-06-06 11:24:54 -0300

    [Criar UIFontExtension] Criar Enum FontSize

---
+++ MarketplaceChallengeTests/Sources/Infrastructure/DesignSystem/UIFont/Enum/FontSize.swift
@@ -0,0 +1,34 @@
+//
+//  FontSize.swift
+//  MarketplaceChallengeTests
+//
+//  Created by Andre Luis Campopiano on 06/06/23.
+//
+
+import Foundation
+
+/// Font size enumeration
+enum FontSize: CGFloat {
+    /// Extra extra large - value: 49
+    case extraExtraLarge = 49
+    /// Extra extra big size - value: 32
+    case extraExtraBig = 32
+    /// extra big size - value: 28
+    case extraBig = 28
+    /// extra large size - value: 24
+    case extraLarge = 24
+    /// large size - value: 20
+    case large = 20
+    /// big size - value: 18
+    case big = 18
+    /// medium size - value: 16
+    case medium = 16
+    /// small size - value: 14
+    case small = 14
+    /// minimum size - value: 12
+    case minimum = 12
+    /// extraMinimum - value: 10
+    case extraMinimum = 10
+    /// extra small - value: 8
+    case extraSmall = 8
+}
