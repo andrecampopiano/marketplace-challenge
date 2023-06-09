88a5e4f02b0222b989f09cf5c83a2a5644a166ad
Andre Campopiano <andre.campopiano@gmail.com>
2023-06-09 12:07:36 -0300

    [HomeViewModel] Criar HomeViewModel
    Criar HomeStatus 
    Criar chamada fetchProductList 
    chamar a metodo fetchProductList da HomeManager
    Criar a propriedade HomeStatus na HomeViewModel

---
+++ MarketplaceChallenge/Sources/Features/Home/Domain/ViewModel/HomeViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  HomeViewModel.swift
+//  MarketplaceChallenge
+//
+//  Created by Andre Luis Campopiano on 09/06/23.
+//
+
+import CoreSwift
+import Foundation
+
+protocol HomeViewModelProtocol {
+    
+    var status: Dynamic<HomeStatus?> { get }
+    
+    func fetchProductList(completion: @escaping ProductListGetCompletion)
+}
+
+class HomeViewModel: HomeViewModelProtocol {
+    
+    // MARK: - Properties
+    var status = Dynamic<HomeStatus?>(nil)
+    
+    private var manager: HomeManagerProtocol?
+    
+    init(manager: HomeManagerProtocol = HomeManager()) {
+        self.manager = manager
+    }
+    
+    // MARK: - Public Methods
+    
+    func fetchProductList(completion: @escaping ProductListGetCompletion) {
+        self.manager?.fetchProductList { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let model):
+                self.handlerSuccess(model: model)
+            case .failure(let error):
+                self.handlerError(error: error)
+            }
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func handlerSuccess(model: ProductListResponse) {
+        
+    }
+    
+    private func handlerError(error: Error) {
+        
+    }
+    
+}
---
+++ MarketplaceChallenge/Sources/Features/Home/Emuns/HomeStatus/HomeStatus.swift
@@ -0,0 +1,14 @@
+//
+//  HomeStatus.swift
+//  MarketplaceChallenge
+//
+//  Created by Andre Luis Campopiano on 09/06/23.
+//
+
+import Foundation
+
+enum HomeStatus {
+    case loading
+    case loaded
+    case error
+}
